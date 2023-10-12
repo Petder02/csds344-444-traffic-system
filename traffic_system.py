2ae733e4df3d663bf2b15703c7e582deb3e6d562
Peter Schlueter <petder02@gmail.com>
2023-10-12 18:12:19 -0400

    Started on basic schema for traffic system
    Added traffic light class, starting on the system class

--- traffic_system.py
+++ traffic_system.py
@@ -6,19 +6,105 @@ class TrafficLight():
 
     """
 
-    def __init__(self, green_time: int, yellow_time: int, red_time: int):
+    def __init__(self, is_green: bool, is_yellow: bool, is_red: bool, delay: float)
         """
         Initialize a traffic light
 
-        state: The state of the traffic light
-        time: The time the traffic light has been in its current state
+        is_green: Is the light green?
+        is_yellow: Is the light yellow?
+        is_red: Is the light red?
+        delay: Amount of time between any light changes
         """
+        self.is_green = is_green
+        self.is_yellow = is_yellow
+        self.is_red = is_red
+        self.delay = delay
+
+
+    def set_green(self):
+        """
+        Set the light to green
+        """
+        self.is_green = True
+        self.is_yellow = False
+        self.is_red = False
+
+
+    def set_yellow(self):
+        """
+        Set the light to yellow
+        """
+        self.is_green = False
+        self.is_yellow = True
+        self.is_red = False
+
+
+    def set_red(self):
+        """
+        Set the light to red
+        """
+        self.is_green = False
+        self.is_yellow = False
+        self.is_red = True
+
+
+    def get_green(self):
+        """
+        Get the green state
+        """
+        return self.is_green
+
+    def get_yellow(self):
+        """
+        Get the yellow state
+        """
+        return self.is_yellow
+
+
+    def get_red(self):
+        """
+        Get the red state
+        """
+        return self.is_red
+
+
+    def get_delay(self):
+        """
+        Get the delay
+        """
+        return self.delay
+
+
+class TrafficSystem():
+    """
+    Class representing a full traffic system
+
+    """
+
+    def __init__(self, north_south: TrafficLight, east_west: TrafficLight,
+                 red_time: float, green_time: float, yellow_time: float):
+        """
+        Initialize a traffic system
+
+        north_south: Traffic light for north/south direction
+        east_west: Traffic light for east/west direction
+        red_time: Time for red light
+        green_time: Time for green light
+        yellow_time: Time for yellow light
+        """
+        self.north_south = north_south
+        self.east_west = east_west
+        self.red_time = red_time
         self.green_time = green_time
         self.yellow_time = yellow_time
-        self.red_time = red_time
 
-    def get_state(self, time: int) -> str:
-        pass
+    
 
-    def __str__(self):
-        return f"Green: {self.green_time}, Yellow: {self.yellow_time}, Red: {self.red_time}"
+
+
+
+
+
+
+
+
