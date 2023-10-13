7c69a1a9c21b40292544b584cd5c0029ea617798
Peter Schlueter <petder02@gmail.com>
2023-10-13 12:40:32 -0400

    Finished up documentation
    Finished documentation and reorganizing, this is more or less finished

--- traffic_system.py
+++ traffic_system.py
@@ -1,8 +1,12 @@
-import time
+import argparse
 import pygame
-import threading
 
-class TrafficLight():
+
+class TrafficLight:
+    """
+    A class representing a traffic light
+
+    """
     def __init__(self, is_green: bool, is_yellow: bool, is_red: bool, delay: float):
         self.is_green = is_green
         self.is_yellow = is_yellow
@@ -36,7 +40,12 @@ class TrafficLight():
     def get_delay(self):
         return self.delay
 
-class PedestrianLight():
+
+class PedestrianLight:
+    """
+    A class representing a pedestrian light
+
+    """
     def __init__(self, walk: bool, stop: bool):
         self.walk = walk
         self.stop = stop
@@ -56,9 +65,15 @@ class PedestrianLight():
         return self.stop
 
 
-class TrafficSystem():
-    def __init__(self, north_light: TrafficLight, south_light: TrafficLight, east_light: TrafficLight, west_light: TrafficLight,
-                 north_cross: PedestrianLight, south_cross: PedestrianLight, east_cross: PedestrianLight, west_cross: PedestrianLight,
+class TrafficSystem:
+    """
+    A class representing a traffic system
+
+    """
+    def __init__(self, north_light: TrafficLight, south_light: TrafficLight, east_light: TrafficLight,
+                 west_light: TrafficLight,
+                 north_cross: PedestrianLight, south_cross: PedestrianLight, east_cross: PedestrianLight,
+                 west_cross: PedestrianLight,
                  red_time: float, green_time: float, yellow_time: float):
         self.north_light = north_light
         self.south_light = south_light
@@ -72,40 +87,42 @@ class TrafficSystem():
         self.green_time = green_time
         self.yellow_time = yellow_time
 
-    def get_north_south_time_remaining(self, time: float):
-        if self.north_light.get_green():
-            return self.green_time
-        elif self.north_light.get_yellow():
-            return self.yellow_time
-        else:
-            return self.red_time
 
-    def get_east_west_time_remaining(self, time: float):
-        if self.east_light.get_green():
-            return self.green_time
-        elif self.east_light.get_yellow():
-            return self.yellow_time
-        else:
-            return self.red_time
+def initialize_traffic_system(red_time: float, green_time: float, yellow_time: float):
+    """
+    Initializes the traffic system for the GUI
 
-
-def initialize_traffic_system():
+    :param red_time: The time (secs) that the red light is on
+    :param green_time: The time (secs) that the green light is on
+    :param yellow_time: The time (secs) that the yellow light is on
+    """
     # Initialize the traffic lights
-    north_light = TrafficLight(False, False, True, 10.0)  # Start with North red
-    south_light = TrafficLight(False, False, True, 10.0)  # Start with South red
-    east_light = TrafficLight(True, False, False, 10.0)  # Start with East green
-    west_light = TrafficLight(True, False, False, 10.0)  # Start with West green
+    north_light = TrafficLight(False, False, True, 0)  # Start with North red
+    south_light = TrafficLight(False, False, True, 0)  # Start with South red
+    east_light = TrafficLight(True, False, False, 0)  # Start with East green
+    west_light = TrafficLight(True, False, False, 0)  # Start with West green
     north_cross = PedestrianLight(False, True)  # Start with North cross stop
     south_cross = PedestrianLight(False, True)  # Start with South cross stop
     east_cross = PedestrianLight(True, False)  # Start with East cross stop
     west_cross = PedestrianLight(True, False)  # Start with West cross stop
 
     traffic_system = TrafficSystem(north_light, south_light, east_light, west_light, north_cross, south_cross,
-                                   east_cross, west_cross, 5.0, 10.0, 5.0)
+                                   east_cross, west_cross, red_time, green_time, yellow_time)
 
     return traffic_system
 
+
 def update_traffic_lights(cycle_time: float, traffic_system: TrafficSystem):
+    """
+    Updates the traffic lights based on the cycle time
+
+    :param cycle_time: The time (secs) since the start of the cycle
+    :param traffic_system: The traffic system
+    """
+    east_west_green = cycle_time < traffic_system.red_time
+    east_west_yellow = cycle_time < traffic_system.red_time + traffic_system.yellow_time
+    north_south_green = cycle_time < traffic_system.red_time + traffic_system.yellow_time + traffic_system.green_time
+    north_south_yellow = cycle_time < traffic_system.red_time + traffic_system.yellow_time + traffic_system.green_time + traffic_system.yellow_time
     if cycle_time < traffic_system.red_time:
         traffic_system.north_light.set_red()
         traffic_system.south_light.set_red()
@@ -133,6 +150,12 @@ def update_traffic_lights(cycle_time: float, traffic_system: TrafficSystem):
 
 
 def draw_traffic_lights(screen: pygame.Surface, traffic_system: TrafficSystem):
+    """
+    Draws traffic lights based on the state of the traffic lights
+
+    :param screen: The pygame screen
+    :param traffic_system: The traffic system
+    """
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
     GREEN = (0, 255, 0)
@@ -147,7 +170,14 @@ def draw_traffic_lights(screen: pygame.Surface, traffic_system: TrafficSystem):
     pygame.draw.circle(screen, east_light_color, (100, 300), 20)  # East traffic light
     pygame.draw.circle(screen, west_light_color, (500, 300), 20)  # West traffic light
 
+
 def draw_pedestrian_signs(screen: pygame.Surface, traffic_system: TrafficSystem):
+    """
+    Draws pedestrian crossing signs based on the state of the pedestrian crossing lights
+
+    :param screen: The pygame screen
+    :param traffic_system: The traffic system
+    """
     # North
     font = pygame.font.Font(None, 36)
     border_width = 10
@@ -164,7 +194,7 @@ def draw_pedestrian_signs(screen: pygame.Surface, traffic_system: TrafficSystem)
     screen.blit(font.render('north', True, (255, 255, 255)), (square_x + 5, square_y + 5))
     screen.blit(font.render(sign_text, True, sign_color), (square_x + 5, square_y + 40))
 
-    # east
+    # East
     square_x = 375
     square_y = 150
     pygame.draw.rect(screen, (0, 0, 0), (
@@ -177,7 +207,7 @@ def draw_pedestrian_signs(screen: pygame.Surface, traffic_system: TrafficSystem)
     screen.blit(font.render('east', True, (255, 255, 255)), (square_x + 5, square_y + 5))
     screen.blit(font.render(sign_text, True, sign_color), (square_x + 5, square_y + 40))
 
-    # west
+    # West
     font = pygame.font.Font(None, 36)
     square_x = 150
     square_y = 375
@@ -191,7 +221,7 @@ def draw_pedestrian_signs(screen: pygame.Surface, traffic_system: TrafficSystem)
     screen.blit(font.render('west', True, (255, 255, 255)), (square_x + 5, square_y + 5))
     screen.blit(font.render(sign_text, True, sign_color), (square_x + 5, square_y + 40))
 
-    # south
+    # South
     font = pygame.font.Font(None, 36)
     square_x = 375
     square_y = 375
@@ -206,7 +236,14 @@ def draw_pedestrian_signs(screen: pygame.Surface, traffic_system: TrafficSystem)
     screen.blit(font.render(sign_text, True, sign_color), (square_x + 5, square_y + 40))
 
 
-def main():
+def main(red_time: float, green_time: float, yellow_time: float):
+    """
+    Main function that runs the traffic system
+
+    :param red_time: The time (secs) that the red light is on
+    :param green_time: The time (secs) that the green light is on
+    :param yellow_time: The time (secs) that the yellow light is on
+    """
     pygame.init()
 
     screenWidth = 600
@@ -216,15 +253,17 @@ def main():
     pygame.display.set_caption("Traffic System")
     clock = pygame.time.Clock()
 
-    traffic_system = initialize_traffic_system()
+    traffic_system = initialize_traffic_system(red_time, green_time, yellow_time)
 
     screen.fill((255, 255, 255))  # Clear the screen
 
     # Draw roads
-    pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(0, 250, 600, 100))  # Horizontal road
-    pygame.draw.rect(screen, (255, 255, 204), pygame.Rect(0, 295, 600, 10))  # Horizontal road line
-    pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(250, 0, 100, 600))  # Vertical road
-    pygame.draw.rect(screen, (255, 255, 204), pygame.Rect(295, 0, 10, 600))  # Vertical road line
+    GRAY = (200, 200, 200)
+    LIGHT_YELLOW = (255, 255, 204)
+    pygame.draw.rect(screen, GRAY, pygame.Rect(0, 250, 600, 100))         # Horizontal road
+    pygame.draw.rect(screen, LIGHT_YELLOW, pygame.Rect(0, 295, 600, 10))  # Horizontal road line
+    pygame.draw.rect(screen, GRAY, pygame.Rect(250, 0, 100, 600))         # Vertical road
+    pygame.draw.rect(screen, LIGHT_YELLOW, pygame.Rect(295, 0, 10, 600))  # Vertical road line
 
     running = True
     while running:
@@ -234,7 +273,8 @@ def main():
 
         # Update the traffic lights based on elapsed time
         elapsed_time = pygame.time.get_ticks() / 1000  # convert milliseconds to seconds
-        cycle_time = elapsed_time % (traffic_system.red_time + traffic_system.yellow_time + traffic_system.green_time + traffic_system.yellow_time)
+        cycle_time = elapsed_time % (
+                    traffic_system.red_time + traffic_system.yellow_time + traffic_system.green_time + traffic_system.yellow_time)
         update_traffic_lights(cycle_time, traffic_system)
 
         # Draw traffic lights
@@ -248,5 +288,19 @@ def main():
 
     pygame.quit()
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog='Traffic System',
+        description='Simulates a basic traffic system'
+    )
+    parser.add_argument('--red-time', type=float, default=10.0, help='The time (secs) that the red light is on')
+    parser.add_argument('--green-time', type=float, default=10.0, help='The time (secs) that the green light is on')
+    parser.add_argument('--yellow-time', type=float, default=5.0, help='The time (secs) that the yellow light is on')
+
+    args = parser.parse_args()
+    red_time: float = float(args.red_time)
+    green_time: float = float(args.green_time)
+    yellow_time: float = float(args.yellow_time)
+
+    main(red_time, green_time, yellow_time)
